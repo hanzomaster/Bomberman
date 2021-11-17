edcd3b4ca309e399b0605041b043677128f21e61
nguyen <nguyenkdksnb@gmail.com>
2021-11-17 08:54:17 +0700

    Add animation to all monsters and change main structure.

--- src/entities/monsters/Doll.java
+++ src/entities/monsters/Doll.java
@@ -1,23 +1,52 @@
 package entities.monsters;
 
-import entities.Entity;
-import javafx.scene.image.Image;
+import graphics.Sprite;
 
-public class Doll extends Entity {
-  public Doll(int xUnit, int yUnit, Image img) {
-    super(xUnit, yUnit, img);
+public class Doll extends Monster {
+  public Doll(int xUnit, int yUnit) {
+    super(xUnit, yUnit, Sprite.dollLeft1.getFxImage());
     // TODO Auto-generated constructor stub
+  }
+
+  public void move() {
+    int tempX = x, tempY = y;
+    switch (direction) {
+      case 0:
+        tempY = y + velocity;
+        break;
+      case 1:
+        tempY = y + velocity;
+        break;
+      case 2:
+        tempX = x - velocity;
+        break;
+      case 3:
+        tempX = x + velocity;
+        break;
+    }
+
+    this.setX(tempX);
+    this.setY(tempY);
   }
 
   @Override
   public void update() {
-
-
-    x -= 1;
-    y += 1;
-    // TODO Auto-generated method stub
+    move();
+    animate();
+    if (direction == 0)
+      this.setImg(Sprite
+          .movingSprite(Sprite.dollLeft1, Sprite.dollLeft2, Sprite.dollLeft3, animate, timeTransfer)
+          .getFxImage());
+    else if (direction == 1)
+      this.setImg(Sprite.movingSprite(Sprite.dollRight1, Sprite.dollRight2, Sprite.dollRight3,
+          animate, timeTransfer).getFxImage());
+    else if (direction == 2)
+      this.setImg(Sprite.movingSprite(Sprite.dollLeft1, Sprite.dollRight1, Sprite.dollLeft3,
+          animate, timeTransfer).getFxImage());
+    else if (direction == 3)
+      this.setImg(Sprite.movingSprite(Sprite.dollRight1, Sprite.dollLeft2, Sprite.dollRight2,
+          animate, timeTransfer).getFxImage());
 
   }
-
 
 }
--- src/entities/monsters/Oneal.java
+++ src/entities/monsters/Oneal.java
@@ -1,5 +1,52 @@
 package entities.monsters;
 
-public class Oneal {
+import graphics.Sprite;
+
+public class Oneal extends Monster {
+
+    public Oneal(int xUnit, int yUnit) {
+        super(xUnit, yUnit, Sprite.onealLeft1.getFxImage());
+        // TODO Auto-generated constructor stub
+    }
+
+    public void move() {
+        int tempX = x, tempY = y;
+        switch (direction) {
+            case 0:
+                tempY = y + velocity;
+                break;
+            case 1:
+                tempY = y + velocity;
+                break;
+            case 2:
+                tempX = x - velocity;
+                break;
+            case 3:
+                tempX = x + velocity;
+                break;
+        }
+
+        this.setX(tempX);
+        this.setY(tempY);
+    }
+
+    @Override
+    public void update() {
+        move();
+        animate();
+        if (direction == 0)
+            this.setImg(Sprite.movingSprite(Sprite.onealLeft1, Sprite.onealLeft2, Sprite.onealLeft3,
+                    animate, timeTransfer).getFxImage());
+        else if (direction == 1)
+            this.setImg(Sprite.movingSprite(Sprite.onealRight1, Sprite.onealRight2,
+                    Sprite.onealRight3, animate, timeTransfer).getFxImage());
+        else if (direction == 2)
+            this.setImg(Sprite.movingSprite(Sprite.onealLeft1, Sprite.onealRight1,
+                    Sprite.onealLeft3, animate, timeTransfer).getFxImage());
+        else if (direction == 3)
+            this.setImg(Sprite.movingSprite(Sprite.onealRight1, Sprite.onealLeft2,
+                    Sprite.onealRight2, animate, timeTransfer).getFxImage());
+
+    }
 
 }
