ff175e5d26dcfe2e59c4b4992ac99d19c9fca180
nguyen <nguyenkdksnb@gmail.com>
2021-11-18 20:23:54 +0700

    Adding A S D W click input.

--- src/GameFrame/KeyboardInput.java
+++ src/GameFrame/KeyboardInput.java
@@ -33,16 +33,16 @@ public class KeyboardInput {
    * Kiểm tra khi ấn key xuống.
    */
   public void updateKeyPressed(KeyEvent e) {
-    if (e.getCode() == KeyCode.UP) {
+    if (e.getCode() == KeyCode.UP || e.getCode() == KeyCode.W) {
       up = true;
     }
-    if (e.getCode() == KeyCode.DOWN) {
+    if (e.getCode() == KeyCode.DOWN || e.getCode() == KeyCode.S) {
       down = true;
     }
-    if (e.getCode() == KeyCode.LEFT) {
+    if (e.getCode() == KeyCode.LEFT || e.getCode() == KeyCode.A) {
       left = true;
     }
-    if (e.getCode() == KeyCode.RIGHT) {
+    if (e.getCode() == KeyCode.RIGHT || e.getCode() == KeyCode.D) {
       right = true;
     }
     if (e.getCode() == KeyCode.SPACE) {
